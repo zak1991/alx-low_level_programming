bf6d9ae5f9e991d4a99ef756ac4f5b333e022b4a
zak1991 <z_ak91@hotmail.fr>
2023-03-30 16:16:04 -0700

    Always look up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-/**
-*main 
-*return: always 0 (success)
+#include <stdio.h>/**
+*main - A program that prints a line with puts function
+*Return: 0 (Success)  
 */
-int main(void)
+int main (void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}	
+puts ("\"Programming is like building a multilingual puzzle") ;
+
+return (0) ;
+
+}	    
